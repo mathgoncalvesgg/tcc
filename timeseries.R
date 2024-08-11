4f3f5ffe9aa809610d982e73d221ec3b3d09afae
mathgoncalvesgg <matheus.goncalves.graca@gmail.com>
2024-08-11 19:35:02 -0300

    bibliotecas

--- timeseries.R
+++ timeseries.R
@@ -7,3 +7,21 @@
 # Aluno Matheus Gonçalves Graça
 # Número USP 13978990
 ##########################################################################
+
+# Instalação e carregamento de Todos os Pacotes ------------------------
+
+pacotes <- c("readxl","plotly","tidyverse","gridExtra","forecast","TTR",
+             "smooth","tsibble","fable","tsibbledata", "fpp3",
+             "urca","ipeadatar", "measurements", "viridis", "FinTS")
+
+if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+  instalador <- pacotes[!pacotes %in% installed.packages()]
+  for(i in 1:length(instalador)) {
+    install.packages(instalador, dependencies = T)
+    break()}
+  sapply(pacotes, require, character = T)
+} else {
+  sapply(pacotes, require, character = T)
+}
+
+#--------------------------------------------------------------------------
