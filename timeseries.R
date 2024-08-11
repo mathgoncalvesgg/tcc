c3b9a70bda2e61389553adb366edfe7cb19eb043
mathgoncalvesgg <matheus.goncalves.graca@gmail.com>
2024-08-11 20:38:22 -0300

    plotmediamovel

--- timeseries.R
+++ timeseries.R
@@ -150,3 +150,30 @@ ggplotly(
 media_movel = ma(general$Produção, order = 3, centre = TRUE)
 
 view(media_movel)
+
+#DataWrangling para inclusão do vetor media_movel na base generalmm
+
+generalmm <- mutate(general,
+                    media_movel)
+
+#Visualizando a série da produção de fertilizantes com a média móvel
+
+ggplotly(
+  generalmm %>%
+    mutate(Data = as.Date(Data)) %>% 
+    ggplot() + 
+    geom_line(aes(x = Data, y = Produção, color = "Produção")) +
+    geom_line(aes(x = Data, y = media_movel, color = "Média Móvel"), linewidth = 0.5) +
+    labs(color = "Legenda:",
+         x = "Data",
+         y = "Produção de Fertilizantes (em toneladas de produto)") +
+    scale_x_date(date_labels = "%m-%Y", date_breaks = "3 months")+
+    scale_y_continuous(limits = c(450000,900000), labels = scales::comma_format(big.mark = ".")) +
+    theme(axis.text.x = element_text(angle = 90, size = 6),
+          panel.background = element_rect(fill = "white", color = "black"),
+          panel.grid = element_line(color = "grey90"),
+          panel.border = element_rect(color = "black", fill = NA),
+          legend.position = "right")
+) %>% layout(showlegend = TRUE,
+             legend = list(orientation = "v"))
+#----------
