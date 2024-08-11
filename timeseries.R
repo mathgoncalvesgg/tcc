eed1c1be5634078e3e78b75420cd4e5ab15e1b8d
mathgoncalvesgg <matheus.goncalves.graca@gmail.com>
2024-08-10 22:04:18 -0300

    titulo

---
+++ timeseries.R
@@ -0,0 +1,9 @@
+########################################################################
+# Trabalho de Conclusão de Curso (TCC) 
+# Cenário da agroindústria brasileira nos anos 2013/2024 
+
+# Data Science & Analytics
+# USP ESALQ
+# Aluno Matheus Gonçalves Graça
+# Número USP 13978990
+##########################################################################
